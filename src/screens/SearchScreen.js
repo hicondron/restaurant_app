0cb808fc97e3584646633bae1f997b5ad7bc225c
Heather Condron <hcondro@wgu.edu>
2020-01-30 11:25:51 -0500

    Initial commit

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet} from 'react-native';
+
+const SearchScreen = () => {
+    return (
+    <View>
+        <Text>Search Screen</Text>
+    </View>
+    );
+};
+
+
+const styles= StyleSheet.create({});
+
+export default SearchScreen;
